a1802679117317bab772d432ca24ba1eab1c1c3b
Nate <nherbst01@gmail.com>
2018-01-17 18:08:17 -0700

    Added last lift and Claw "PId"

--- Skills Bot/PIDS.c
+++ Skills Bot/PIDS.c
@@ -1,5 +1,4 @@
 #include "Voids & Vars.c"
-
 //----------------------Drive PID----------------------//
 
 static float  Drive_Kp = 0.30; //Power Tuning Value
@@ -56,6 +55,127 @@ task DriveController()
 		setDrivePower(drivepowerPID);
 
 		lastDriveError = DriveError;
+		// Don't hog cpu
+		wait1Msec( 25 );
+	}
+}
+//----------------------LIFT PID----------------------//
+
+
+static float  liftR_Kp = 0.2750; 	//Power Tuning Value
+static float  liftRRequestedValue;
+static float  liftR_Kd = 0.6;			// Requested Guess Value
+
+float liftRD;
+float liftRP;
+float lastliftRError;
+float liftRDF;
+
+//lift Values Right
+//Clamp = 450
+//Straight = 2000
+//Locked = 3850
+//V = 1150
+
+//lift Values Left
+//Clamp = 4000
+//Straight = 2600
+//Locked = 1250
+//V = 3600
+task liftRController()
+{
+	float  liftRSensorCurrentValue;
+	float  liftRError;
+	float  liftRDrive;
+
+	while( true )
+	{
+		// Read the sensor value and scale
+		liftRSensorCurrentValue = SensorValue[ liftPot ];
+
+		// calculate error
+		liftRError =  liftRRequestedValue - liftRSensorCurrentValue;
+
+		// calculate drive
+		liftRP = (liftR_Kp * liftRError);
+
+		liftRD = liftRError- lastliftRError;
+		liftRDF = (liftR_Kd * liftRD);
+
+		liftRDrive = liftRP + liftRDF;
+
+		// limit drive
+		if( liftRDrive > 127 )
+			liftRDrive = 127;
+		if( liftRDrive < (-127) )
+			liftRDrive = (-127);
+
+		// send to motor
+
+		setLiftPower(127);
+
+		lastliftRError = liftRError;
+
+		// Don't hog cpu
+		wait1Msec( 25 );
+	}
+}
+//----------------------Mobile Bar PID----------------------//
+
+static float  calw_Kp = 0.30; //Power Tuning Value
+static float  calwRequestedValue;
+static float  calw_Kd = 0.5; // Requested Guess Value
+
+float calwD;						//Establishing variables
+float calwP;
+float lastcalwError;
+float calwDF;
+int waitMobliERerror = 100;
+float  calwSensorCurrentValue;
+
+//mobile Values Right
+//Clamp = 450
+//Straight = 2000
+//Locked = 3850
+//V = 1150
+
+//mobile Values Left
+//Clamp = 4000
+//Straight = 2600
+//Locked = 1250
+//V = 3600
+task calwController()
+{
+	//float  calwSensorCurrentValue;
+	float  calwError;
+	float  calwDrive;
+
+	while( true )
+	{
+		// Read the sensor value and scale
+		calwSensorCurrentValue = SensorValue[ clawp ];
+		// calculate error
+		calwError =  calwRequestedValue - calwSensorCurrentValue;
+
+		// calculate drive
+		calwP = (calw_Kp * calwError);
+
+		calwD = calwError- lastcalwError;
+		calwDF = (calw_Kd * calwD);
+
+		calwDrive = calwP + calwDF;
+
+		// limit drive
+		if( calwDrive > 127 )
+			calwDrive = 127;
+		if( calwDrive < (-127) )
+			calwDrive = (-127);
+
+		// send to motor
+
+		motor[ Calw ] = calwDrive;
+
+		lastcalwError = calwError;
 		// Don't hog cpu
 		wait1Msec( 25 );
 	}
