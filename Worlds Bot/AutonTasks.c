37bad08eeab46f92a92ff790c20c7bb8e28d088b
Nate <nherbst01@gmail.com>
2018-03-01 09:27:20 -0700

    Converted to inches

---
+++ Worlds Bot/AutonTasks.c
@@ -0,0 +1,524 @@
+#include "BackOver Functions 2017-18.c"
+#include "Drive PID.c"
+//#region Variables
+//-------------Variables--------------//
+//Variables for turn and drive
+int Nine = 750;
+int full = 1750;
+int drivepower = 127;
+int NineP = 933;
+int FullP = 1860;
+
+// PID Mobile Variables
+int mobileIn = 1051;
+int mobileOut = 3340;
+int mobileMid = 2456;
+int mobileSMid = 2770;
+
+// PID Lift variables
+int BottomLift = 2290;
+int TopLift = 855;
+int ParallelLift = 1880;
+int SkillsLift = 2140;
+
+// Auto Stack teir variables
+int teirHeightOneTwo = 2138;
+int teirHeightThree = 2020;
+int teirHeightFour = 1902;
+int teirHeightFive = 1720;
+int teirHeightSix = 1596;
+int teirHeightSeven = 1540;
+int teirHeightEight = 1460;
+int teirHeightNine = 1340;
+int teirHeightTen = 1210;
+int teirHeightEleven = 1110;
+int teirHeightTwelve = 940;
+int teirHeightThirteen = 940;
+int teirHeightFourteen = 940;
+int teirHeightFifteen = 940;
+
+float wheelDiameter = 3;
+
+int InchesToCounts(float value) //converts drive encoder counts into inches
+{
+  return (value * 360)/(PI * wheelDiameter);
+}
+//#endregion
+//#region Autonomous Voids
+//--------------------Autonomous Voids-----------------------//
+void AutoStrait(int distance, int power)
+ // Creating a function to make sure the robot drives forward
+{
+	int direction = sgn(distance);
+	distance = abs(distance);
+
+ // telling it to do this while you are driving to the disired distance
+	while((abs(SensorValue[rightEncoder])/2) < distance)
+	{
+		 // Checking if the right drive is faster than the left drive
+		if (abs(SensorValue[rightEncoder]) > abs(SensorValue[leftEncoder]))
+		{
+			setDrivePower(direction * (power + 4), direction * (power - 10));
+		}
+		// Checking if the left drive is faster than the right drive
+		else if (abs(SensorValue[leftEncoder]) > abs(SensorValue[rightEncoder]))
+		{
+			setDrivePower(direction * (power - 2), direction * power);
+		}
+		else //if the values are the same, keeps motor powers equall
+		{
+			setDrivePower(direction * power, direction * power);
+		}
+	}
+		motor[LDrive] = 0; // stops motors when finished with function
+		motor[RDrive] = 0;
+}
+
+void Atondrive(float inches, int power)
+{ //Aton function to move forward by its self
+	int ticks = InchesToCounts(inches);
+	  //Clear Encoders
+  SensorValue[rightEncoder] = 0;
+  SensorValue[leftEncoder] = 0;
+		///forward
+	AutoStrait(ticks*0.6, power);
+	AutoStrait(ticks*0.8, power/2);
+	AutoStrait(ticks, power/3);
+//sudden stop to correct overshooting the value
+	motor[LDrive] = -20 * sgn(ticks);
+	motor[RDrive] = -20 * sgn(ticks);
+	wait1Msec(100);
+}
+//#endregion
+//#region User Contoll Tasks
+task drive() //Redirecting Drive into a task
+{
+	while(true)
+	{
+	setDrivePower(vexRT[Ch3],vexRT[Ch2]);
+	}
+}
+
+task IntakeCone()
+{
+	setIntakePower (127);
+	wait1Msec (25);
+	setIntakePower (20);
+}
+
+task OutakeCone()
+{
+setIntakePower (-127);
+wait1Msec (1000);
+setIntakePower (20);
+}
+
+task OutakePreload()
+{
+	liftRRequestedValue = SkillsLift + 500;
+	wait1Msec(100);
+	setIntakePower (-127);
+	wait1Msec(300);
+	setIntakePower (20);
+	liftRRequestedValue=2000;
+}
+
+task AutoStackUpSimple()									// Simple Auto stack cone up
+{
+	liftRRequestedValue = ParallelLift;
+
+	setChainPower(-127);
+	wait1Msec(500);
+	setChainPower(-10);
+
+	liftRRequestedValue = SkillsLift + 400;
+
+	setIntakePower(127);
+	wait1Msec(400);
+	setIntakePower(20);
+	wait1Msec(100);
+
+	liftRRequestedValue = ParallelLift;
+
+	setChainPower(127);
+	wait1Msec(750);
+	setChainPower(20);
+
+	liftRRequestedValue = SkillsLift + 500;
+	wait1Msec(400);
+	setIntakePower(-127);
+	wait1Msec(200);
+	liftRRequestedValue = ParallelLift;
+	wait1Msec(300);
+	setIntakePower(10);
+}
+//#endregion
+//#region Special Auto Stack Voids
+//--------------------Special Auto Stack Voids-----------------------//
+void AutoStackUp()									// Simple Auto stack cone up
+{
+	liftRRequestedValue = ParallelLift;
+
+	setChainPower(-127);
+	wait1Msec(500);
+	setChainPower(-10);
+
+	liftRRequestedValue = SkillsLift + 400;
+
+	setIntakePower(127);
+	wait1Msec(1000);
+	setIntakePower(20);
+	wait1Msec(100);
+
+	liftRRequestedValue = ParallelLift;
+
+	setChainPower(127);
+	wait1Msec(750);
+	setChainPower(20);
+
+	liftRRequestedValue = SkillsLift + 500;
+	wait1Msec(400);
+	setIntakePower(-127);
+	wait1Msec(200);
+	liftRRequestedValue = ParallelLift;
+	wait1Msec(300);
+	setIntakePower(10);
+}
+/*
+void AutoStackDown()									// Simple Auto stack cone down
+{
+//	Atondrive(1000, -100);
+}
+*/
+//_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_//
+//#endregion
+//--------------------Autonomous Programs----------------------------//
+//#region Driver Program Programs
+void Preload()
+{
+}
+//#endregion
+//#region Score 5's
+void RightFive()
+{
+}
+
+void LeftFive()
+{
+}
+//#endregion
+//#region Score 10's
+void RightTen()
+{
+}
+
+void LeftTen()
+{
+}
+//#endregion
+//#region Score 20's
+void RightTwenty() // Auton task to grab moble base on the right and score it
+{
+}
+
+void LeftTwenty()
+{
+}
+//#endregion
+//#region Score Special's
+void SkillsOne ()
+{
+	//------------------------------------------------------------//
+			setIntakePower(15);                   //
+			liftRRequestedValue = 2000;
+			wait1Msec(700);
+			mobileRRequestedValue = mobileOut;
+
+			setChainPower(20);
+
+			Atondrive(610, 127);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(200);
+
+			Atondrive(-550, 127);
+			liftRRequestedValue = 2600;
+			wait1Msec(300);
+			setIntakePower(-127);                   //
+			liftRRequestedValue = 2000;
+
+			TurnPID(-NineP/2, true);
+			setIntakePower(0);                   //
+
+			Atondrive(-290, 127);
+
+			TurnPID(-NineP-30, true);
+
+			Atondrive(285, 127);
+
+			setDrivePower(40,40);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+
+			mobileRRequestedValue = mobileIn;
+			//------------------------------------------- Place 2nd
+			Atondrive(-230, drivepower);
+
+			TurnPID(-NineP, true);
+
+			Atondrive(170, drivepower);
+
+			mobileRRequestedValue = mobileOut;
+
+			TurnPID(-NineP-15, true);
+
+			//mobileRRequestedValue = mobileOut;
+			//WaitieThing();
+
+			/*setDrivePower(-30,-30);
+			wait1Msec(1200);*/
+
+			Atondrive(320, drivepower);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(400);
+			Atondrive(-260, drivepower);
+
+			TurnPID(-FullP+5, true);
+
+			Atondrive(115, 127);
+
+			//setDrivePower(40,40);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+			//------------------------------------------- Place 3rd
+			Atondrive(-535, drivepower);
+
+			mobileRRequestedValue = mobileMid;
+
+			TurnPID(NineP-5, true);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+
+			Atondrive(420, drivepower);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(500);
+
+			TurnPID(-NineP-30, true);
+
+			Atondrive(590, drivepower);
+			//setDrivePower(40,40);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+			Atondrive(-100, drivepower);
+			mobileRRequestedValue = mobileMid-100;
+			//setDrivePower(40,40);
+			wait1Msec(300);
+
+			//Atondrive(-170, drivepower);
+
+			//------------------------------------------- Place 4rd
+			TurnPID(FullP+35, true);
+
+			mobileRRequestedValue = mobileOut;
+
+			//////////////
+			//setDrivePower(-80,-80);
+			//wait1Msec(2000);
+			//////////////
+
+			Atondrive(760, drivepower);
+
+			mobileRRequestedValue = mobileMid;
+
+			Atondrive(675, drivepower);
+
+			mobileRRequestedValue = mobileOut;
+			setDrivePower(20,20);
+			WaitieThing();
+			//------------------------------------------- Place 5th
+			Atondrive(-545, drivepower);
+
+			mobileRRequestedValue = mobileMid;
+
+			TurnPID(NineP-10, true);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+
+			Atondrive(460, drivepower);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(500);
+
+			TurnPID(-NineP-42, true);
+
+			Atondrive(565, drivepower);
+			//setDrivePower(20,20);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+			Atondrive(-120, drivepower);
+			//------------------------------------------- Place 6th
+			TurnPID(1320 , true);
+
+			Atondrive(620, 127);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(200);
+
+			Atondrive(-585, 127);
+
+			TurnPID(-NineP/2, true);
+			setIntakePower(0);                   //
+
+			Atondrive(-250, 127);
+
+			TurnPID(-NineP-30, true);
+
+			Atondrive(285, 127);
+
+			setDrivePower(40,40);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+
+			Atondrive(-10, 127);
+
+			mobileRRequestedValue = mobileIn;
+
+			Atondrive(-230, drivepower);
+			//------------------------------------------- Place 7th
+			mobileRRequestedValue = mobileOut;
+			TurnPID(-1220, true);
+
+			Atondrive(857, drivepower);
+
+			mobileRRequestedValue = mobileIn;
+			wait1Msec(600);
+
+			Atondrive(-730, drivepower);
+
+			TurnPID(FullP-200, true);
+
+			//Atondrive(-210, drivepower);
+
+			//TurnPID(-NineP-10, true);
+
+			setDrivePower(60,60);
+
+			mobileRRequestedValue = mobileOut;
+			WaitieThing();
+
+			Atondrive(-200, drivepower);
+
+}
+
+void SkillsTwo ()
+{
+}
+//#endregion
+//#region Other Programs
+void Defensive ()
+{
+	setIntakePower(15);                   //
+	liftRRequestedValue = 2000;
+	wait1Msec(700);
+	mobileRRequestedValue = mobileIn;
+
+	setChainPower(20);
+
+	Atondrive(100, drivepower);
+
+	TurnPID(NineP/2,true);
+
+//	mobileRRequestedValue = mobileOut;
+
+	Atondrive(600*1.25, drivepower);
+
+	Atondrive(-150*1.25 , drivepower);
+	mobileRRequestedValue = mobileOut;
+	TurnPID(NineP/2-50,true);
+	wait1Msec(250);
+
+	Atondrive(300, drivepower);
+
+	mobileRRequestedValue = mobileIn;
+	WaitieThing();
+	liftRRequestedValue = SkillsLift + 350;
+	wait1Msec(200);
+
+	setIntakePower(-127);                   //
+	wait1Msec(300);
+	setIntakePower(0);
+	liftRRequestedValue = 2000;                //
+
+}
+
+void DoNothing ()
+{
+	setIntakePower(15);                   //
+	liftRRequestedValue = 1500;
+	wait1Msec(100);
+
+	Atondrive(1000000000, 127);
+	Atondrive(-1000000000, 127);
+
+}
+
+void DefensiveTwo ()
+{
+
+		setIntakePower(15);                   //
+		liftRRequestedValue = 2000;
+		wait1Msec(700);
+		mobileRRequestedValue = mobileIn;
+
+		setChainPower(20);
+
+		Atondrive(140, drivepower);
+
+		TurnPID(-NineP/2,true);
+
+	//	mobileRRequestedValue = mobileOut;
+
+		Atondrive(600*1.25, drivepower);
+
+		Atondrive(-150*1.25 , drivepower);
+		mobileRRequestedValue = mobileOut;
+		TurnPID(-NineP/2+100,true);
+		wait1Msec(250);
+
+		Atondrive(300, drivepower);
+
+		mobileRRequestedValue = mobileIn;
+		WaitieThing();
+		liftRRequestedValue = SkillsLift + 350;
+		wait1Msec(200);
+
+		setIntakePower(-127);                   //
+		wait1Msec(300);
+		setIntakePower(0);                   //
+		liftRRequestedValue = 2000;
+}
+
+void DoNothingTwo ()
+{
+}
+void SpecialAton()
+{
+	setDrivePower(127 , 127);
+	wait1Msec(5000);
+	setDrivePower(0 , 0);
+}
+//_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_//
+//#endregion
+//<editor-fold
+
+
+//</editor-fold>
---
+++ Worlds Bot/Drive PID.c
@@ -0,0 +1,73 @@
+int driveStraightError = 100;
+//#region Turn PID
+//----------------------Turn PID----------------------//
+
+static float  DriveT_Kp = 0.39; 	//Power Tuning Value
+static float  DriveTRequestedValue;
+static float  DriveT_Kd = 2.5;			// Requested Guess Value
+
+float DriveTD;
+float DriveTP;
+float lastDriveTError;
+float DriveTDF;
+
+task TPIDDriveController()
+{
+  float  DriveTSensorCurrentValue;
+	float  DriveTError;
+	float  DriveTDrive;
+
+  	while( true )
+  	{
+  		// Read the sensor value and scale
+  		DriveTSensorCurrentValue = SensorValue[ Gyro ];
+
+  		// calculate error
+  		DriveTError =  DriveTRequestedValue - DriveTSensorCurrentValue;
+
+  		// calculate drive
+  		DriveTP = (DriveT_Kp * DriveTError);
+
+  		DriveTD = DriveTError- lastDriveTError;
+  		DriveTDF = (DriveT_Kd * DriveTD);
+
+  		DriveTDrive = DriveTP + DriveTDF;
+
+  		// limit drive
+  		if( DriveTDrive > 127 )
+  			DriveTDrive = 127;
+  		if( DriveTDrive < (-127) )
+  			DriveTDrive = (-127);
+
+  		// send to motor
+
+  		setDrivePowerLeft(-DriveTDrive);
+      setDrivePowerRight(DriveTDrive);
+
+  		lastDriveTError = DriveTError;
+
+  		// Don't hog cpu
+  		wait1Msec( 25 );
+  	}
+}
+//#endregion
+//#region Called functions
+
+void TurnPID (int turnAmount, bool waity=false)
+{
+  startTask(TPIDDriveController);
+
+  SensorValue[Gyro] = 0;
+  DriveTRequestedValue = turnAmount;
+  if (waity)
+  {
+    //  distance = abs(distance); //help
+      while( SensorValue[ Gyro ] >= DriveTRequestedValue + driveStraightError
+  		|| SensorValue[ Gyro ] <= DriveTRequestedValue - driveStraightError){}
+      wait1Msec(200);
+      stopTask(TPIDDriveController);
+  }
+  wait1Msec(25);
+  stopTask(TPIDDriveController);
+}
+//#endregion
